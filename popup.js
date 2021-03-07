f69e500bb50c0f1a9b21adb9424fdf2c1c045576
Divya Srivastava <srivastava.divya7@gmail.com>
2021-03-08 04:12:02 +0530

    code clean: separate file

--- popup.js
+++ popup.js
@@ -1,5 +1,3 @@
-// var port = chrome.runtime.connect({name: "extension.nexialautomation"});
-// // port.postMessage({joke: "Knock knock"});
 const inspec_btn = document.getElementById("inspectAction");
 var testCaseList ;
 
@@ -40,80 +38,7 @@ function inspectCaption(inspect_btn_caption, is_inspecting) {
 //   array.splice(index, 1);
 // }
 
-function tableFromJson(inspectElementList) {
-  var col = [];
-  for (var i = 0; i < inspectElementList.length; i++) {
-    for (var key in inspectElementList[i]) {
-      if (col.indexOf(key) === -1) {
-        col.push(key);
-      }
-    }
-  }
 
-  // Create a table.
-  var table = document.createElement("table");
-  table.setAttribute('class', 'table table-hover table-bordered table-responsive-md text-center');
-
-  // Create table header row using the extracted headers above.
-  var tr = table.insertRow(-1);                   // table row.
-
-  for (var i = 0; i < col.length; i++) {
-    var th = document.createElement("th");      // table header.
-    th.innerHTML = col[i];
-    tr.appendChild(th);
-  }
-
-  // add json data to the table as rows.
-  for (var i = 0; i < inspectElementList.length; i++) {
-
-    tr = table.insertRow(-1);
-
-    for (var j = 0; j < col.length; j++) {
-      var tabCell = tr.insertCell(-1);
-
-      if (col[j] === "other") {
-        var button = document.createElement('button');
-        button.innerHTML = 'Delete';
-        button.onclick = function(e) {
-        // alert('here be dragons');return false;
-        console.log(i, '  == i ')
-        inspectElementList.splice(i-1, 1);
-        table.deleteRow(i);
-        console.log(inspectElementList, '  inspectElementList')
-      };
-      tabCell.appendChild(button);
-      // tabCell.innerHTML = "<button type='button' onclick='deleteInspect(inspectElementList,i)'>Delete</button>"
-      } else if(col[j] === 'locator') {
-        //Create and append select list
-        var selectList = document.createElement("select");
-        selectList.id = "mySelect";
-        // tabCell.appendChild(selectList);
-        console.log(inspectElementList[i][col[j]])
-        const locatorList = inspectElementList[i][col[j]]; 
-        console.log(typeof locatorList)
-        //Create and append the options
-        for (var index = 0; index < locatorList.length; index++) {
-          var option = document.createElement("option");
-          option.value = locatorList[index];
-          option.text = locatorList[index];
-          selectList.appendChild(option);
-        }
-        tabCell.appendChild(selectList);
-      } else {
-        tabCell.setAttribute('class', 'pt-3-half');
-        tabCell.setAttribute('contenteditable', 'true');
-        tabCell.innerHTML = inspectElementList[i][col[j]];
-      }
-    }
-  }
-
-  // Now, add the newly created table with json data, to a container.
-  var divShowData = document.getElementById('showData');
-  divShowData.innerHTML = "";
-  divShowData.appendChild(table);
-      
-  // document.getElementById('msg').innerHTML = '<br />You can later <a href="https://www.encodedna.com/javascript/dynamically-add-remove-rows-to-html-table-using-javascript-and-save-data.htm" target="_blank" style="color:#1464f4;text-decoration:none;">get all the data from table and save it in a database.</a>';
-}
 
 document.getElementById('url').addEventListener('focusout', function(e) {
   // checking is url validated
