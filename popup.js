4dc4dcf3a04e5bb17a7758c3cfce1e8b58b04c5f
Divya Srivastava <srivastava.divya7@gmail.com>
2021-03-09 04:24:47 +0530

    resolve : site url regex

--- popup.js
+++ popup.js
@@ -31,10 +31,21 @@ function inspectCaption(inspect_btn_caption, is_inspecting) {
   }
 }
 
+function validURL(myURL) {
+  var pattern = new RegExp('^(http(s)?:\/\/.)'+ // protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ //port
+  '(\\?[;&amp;a-z\\d%_.~+=-]*)?'+ // query string
+  '(\\#[-a-z\\d_]*)?$','i');
+  return pattern.test(myURL);
+}
+
 document.getElementById('url').addEventListener('focusout', function(e) {
   // checking is url validated
-  var res = (e.target.value).match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-  if(res == null) {
+  var res = validURL(e.target.value);
+
+  if(!res) {
     console.log(document.getElementsByClassName('valid-feedback')[0])
     document.getElementsByClassName('valid-feedback')[0].classList.add("d-block");
     e.target.value = '';
