9564e74f0ca5ca78e3c7be22a0f147337f67fc58
Divya Srivastava <srivastava.divya7@gmail.com>
2021-04-15 15:20:31 +0530

    - add() : element as disable - add() : Enable element on edit mode - remove() : varable, console and unused code - change() : var to let - add() : function to toggle element enable disable - change() : update param logic

--- view/create_table.js
+++ view/create_table.js
@@ -1,7 +1,13 @@
-let cmd_selected = '';
-let updatedObject = {};
-let cmd_param_length = 0;
-let table;
+let table, updatedObject = {}, cmd_param_length = 0;
+
+function updateInspectList(i) {
+
+}
+
+function toggleElement(element, enable) {                                         // Enable disable Element
+  if(!enable) element.setAttribute("disabled", "true");
+  else element.removeAttribute("disabled");
+}
 
 function deleteSubTable(tableIndex) {
   let inspectTable = document.getElementById('table_' + tableIndex);
@@ -13,23 +19,26 @@ function deleteSubTableRow(tableIndex, rowIndex) { document.getElementById('tabl
 function createSubTableRow(param_table, key, data, i, editable) {
   // console.log('KEY ______ ', key, '    VALUE _________ ', data)
   let tr = param_table.insertRow(-1);
-
   let keyCell = tr.insertCell(-1), valueCell = tr.insertCell(-1);
   keyCell.innerHTML = '<span class="param-idx">' + + key.replace("param", "") + '</span>';
 
-  let valueCellText = '';
-  if (Object.keys(data).length !== 0) { valueCellText = data[key][0]; }
-  if (valueCellText && valueCellText.length > 20) { valueCellText = valueCellText.substring(0,20) + '...';}
-  valueCell.setAttribute('id', (key + '_' + i))
-  if (editable) {
-    valueCell.innerHTML="<input type='text' id='input_" + i + "' value='" + valueCellText + "'>";
-  } else valueCell.innerHTML = valueCellText
+  let element = '';
+  if (data.length <= 1) {                                 // param is other than locator
+    element = document.createElement("INPUT");
+    element.setAttribute("type", "text");
+    element.setAttribute("value", data[0]);
+    element.setAttribute('disabled', editable)
+  } else if (data.length > 1) {                           // param is locator
+    element = createSelectElement(data, editable)
+  }
+  element.setAttribute('id', (key + '_' + i))
+  valueCell.appendChild(element);
 }
 
 function getCommandParam(str) {
   // console.log('command === ', str)
   // ToDo : (Case) Command is undefined and has no param
-  var arr = str.substring(
+  let arr = str.substring(
     str.lastIndexOf("(") + 1,
     str.lastIndexOf(")")
   ).split(',');
@@ -38,59 +47,33 @@ function getCommandParam(str) {
 }
 
 function updateParamRow(i) {
-  // console.log(updatedObject, ' --------------- JSON TO EDIT -----------------')
-  var parameterArr = getCommandParam(updatedObject.command)
-  // var size = Object.size(updatedObject.command);
-  // console.log('cmd_param_length =  ', cmd_param_length, parameterArr.length)
-  if(parameterArr.length < cmd_param_length) {
-    // deleteSubTableRow(i)
+  let parameterArr = getCommandParam(updatedObject.command)
+
+  if(parameterArr.length < cmd_param_length) {                                 // Delete extra param row from sub table
     for (let index = cmd_param_length; index > parameterArr.length; index--) {
-      // console.log(index, parameterArr.length)
       deleteSubTableRow(i, index-1)
     }
-    // console.log('DELETE ROW ')
   }
   cmd_param_length = parameterArr.length;
   for (let index = 1; index <= (parameterArr.length); index++) {
-    var el = document.getElementById('param'+ index + '_' + i)
-    // param['param'+index] = [el, el.innerHTML];
-    if(el) {el.innerHTML = '';}
+    let el = document.getElementById('param'+ index + '_' + i)
+    if(el) { el.removeAttribute("disabled"); }
     else {
-      createSubTableRow(document.getElementById('table_' + i), ('param'+ index), {}, i, true);
+      createSubTableRow(document.getElementById('table_' + i), ('param'+ index), [], i, true);
       el = document.getElementById('param'+ index + '_' + i)
     }
-    // else create sub table row;
-    if (parameterArr[index - 1] === 'locator') {
-      const locatorList = createSelectElement(inspectElementList[i].param['param1'])
-      el.appendChild(locatorList)
-      el.onchange = function(e) {
-        updatedObject.param['param1'] = e.target.value;
-      }
-    } else {
-      var inputVal = ''
-      if ((inspectElementList[i].param).hasOwnProperty('param'+index)) {
-        inputVal = inspectElementList[i].param['param'+index][0]
-      }
-      // var inputVal = inspectElementList[i].param['param'+index][0] ? inspectElementList[i].param['param'+index][0] : '';
-      if (el) { el.innerHTML="<input type='text' id='input_" + i + "' value='" + inputVal + "'>"; }
-    }
   }
-  // console.log(updatedObject , '----------------- updatedObject')
 }
 
 function editRow(i) {
-  toggleRowEdit(i, false);
+  toggleRow(i, false);
 
-  // console.log(inspectElementList[i], '###########################')
-  const cmdList = createSelectElement(cmd)
   let cmd_el = document.getElementById('command_' + i);
-  cmd_el.innerHTML = '';
-  cmd_el.appendChild(cmdList);
+  cmd_el.removeAttribute("disabled");
   cmd_el.onchange = function (e) {
     updatedObject.command = e.target.value;
     cmd_el.value = e.target.value;
     updateParamRow(i);
-    // console.log('data change', e.target.value)
   }
 
   updateParamRow(i)
@@ -98,24 +81,24 @@ function editRow(i) {
 }
 
 function saveRow(i) {
-  toggleRowEdit(i, true);
+  toggleRow(i, true);
+  let cmdElement = document.getElementById('command_' + i);
+  toggleElement(cmdElement, false)
+  inspectElementList[i].command = cmdElement.value                                // Update inspectElementList's command
 
-  const data = updatedObject.param;
-
-  let cmd_el = document.getElementById('command_' + i);
-  // var target = document.getElementById('target_' + i);
-  // var input = document.getElementById('input_' + i);
-  cmd_el.innerHTML = updatedObject.command;
+  const paramList = getCommandParam(cmdElement.value);                            // Update inspectElementList's param
   let index = 1;
-  for (let key in data) {
-    document.getElementById('param' + index + '_' + i).innerHTML = data[key][0];
+  (paramList).forEach(element => {
+    const paramElement = document.getElementById('param'+ index + '_' + i);
+    if(element === 'locator') {
+      // ToDo
+    } else inspectElementList[i].param['param' + index] = [paramElement.value];
+    toggleElement(paramElement, false)
     index++;
-  }
-  // console.log(cmd_selected, target_selected , input_data)
+  });
 }
 
-
-function toggleRowEdit(/*Number*/i, /*Boolean*/enable) {
+function toggleRow(/*Number*/i, /*Boolean*/enable) {
   document.getElementById("delete_" + i).style.display = enable ? "inline-block" : "none";
   document.getElementById("edit_" + i).style.display = enable ? "inline-block" : "none";
   document.getElementById("save_" + i).style.display = enable ? "none" : "inline-block";
@@ -127,15 +110,15 @@ function createDuplicateButton() {
 }
 
 function createDeleteButton(i) {
-  var button = document.createElement('button');
+  let button = document.createElement('button');
   button.setAttribute('class', 'btn text-dark delete-button ripple-surface')
   button.setAttribute('id',('delete_' + i));
   button.innerHTML = '<i class="fa fa-trash"></i>';
   button.onclick = function(e) {
-    // console.log(i, '  == i ')
-    inspectElementList.splice(i-1, 1);
-    table.deleteRow(i-1);
-    // console.log(inspectElementList, '  inspectElementList')
+    // Todo Update background.js
+    inspectElementList.splice(i, 1);
+    table.deleteRow(i + 1);
+    console.log(inspectElementList, '  inspectElementList')
   };
   return button;
 }
@@ -146,9 +129,6 @@ function createEditButton(i) {
   button.setAttribute('id',('edit_' + i));
   button.innerHTML = '<i class="fa fa-edit"></i>';
   button.onclick = function(e) {
-    // console.log(i, '  == i ')
-    // editObject.command = inspectElementList[i].command;
-    // editObject.param = inspectElementList[i].param;
     updatedObject.command = inspectElementList[i].command;
     updatedObject.param = inspectElementList[i].param;
     cmd_param_length = getCommandParam(inspectElementList[i].command).length;
@@ -159,17 +139,13 @@ function createEditButton(i) {
 }
 
 function createSaveButton(i) {
-  var button = document.createElement('button');
+  let button = document.createElement('button');
   button.setAttribute('class', 'btn text-dark')
   button.setAttribute('id',('save_' + i));
   button.setAttribute('style',('display: none'));
   button.innerHTML = '<i class="fa fa-check"></i>';
   button.onclick = function(e) {
-    // console.log(i, '  == i ')
     saveRow(i)
-    // inspectElementList.splice(i-1, 1);
-    // table.deleteRow(i);
-    // console.log(inspectElementList, '  inspectElementList')
   };
   return button;
 }
@@ -181,29 +157,31 @@ function createCloseButton(i) {
   button.setAttribute('style',('display: none'));
   button.innerHTML = '<i class="fa fa-times"></i>';
   button.onclick = function(e) {
-    document.getElementById('command' + '_' + i).innerHTML = inspectElementList[i].command;
+    document.getElementById('command' + '_' + i).value = inspectElementList[i].command;
+    toggleElement(document.getElementById('command' + '_' + i), false)
     deleteSubTable(i);
     const data = inspectElementList[i].param;
-    for (let key in data) { createSubTableRow(document.getElementById('table_' + i), key, data, i, false); }
-    toggleRowEdit(i, true);
+    for (let key in data) { createSubTableRow(document.getElementById('table_' + i), key, data[key], i, false); }
+    toggleRow(i, true);
   };
   return button;
 }
 
-function createSelectElement(items) {
+function createSelectElement(items, editable = true) {
   // Create and append select list
-  var selectList = document.createElement("select");
-  selectList.id = "mySelect";
+  let selectList = document.createElement("select");
+  selectList.setAttribute('class', 'form-control')
+  if (!editable) selectList.setAttribute('disabled', 'true')
   // console.log(items)
 
   //Create and append the options
-  var optgroup = '';
-  for (var index = 0; index < items.length; index++) {
+  let optgroup = '';
+  for (let index = 0; index < items.length; index++) {
     if ((items[index]).includes('=')) {
       optgroup = document.createElement("optgroup");
       optgroup.setAttribute('label', items[index].split('=')[0].toUpperCase())
     }
-    var option = document.createElement("option");
+    let option = document.createElement("option");
     option.value = items[index];
     option.text = items[index];
     if(optgroup) {
@@ -219,27 +197,18 @@ function createSubTable(data, i) {
   const param_table = document.createElement("table");
   param_table.setAttribute('class', 'sub-table');
   param_table.setAttribute('id', 'table_' + i);
-  // console.log(i)
-  // console.log(data, 'Sub table data')
+
   for (let key in data) {
-    createSubTableRow(param_table, key, data, i, false)
-    // var tr = param_table.insertRow(-1);
-    // var keyCell = tr.insertCell(-1), valueCell = tr.insertCell(-1);
-    // var valueCellText = data[key][0];
-    // if (valueCellText && valueCellText.length > 20) { valueCellText = valueCellText.substring(0,20) + '...';}
-    // keyCell.innerHTML = key;
-    // valueCell.setAttribute('id', (key + '_' + i))
-    // valueCell.innerHTML = valueCellText
-    // console.log('KEY === ', key, 'DATA === ', data[key])
+    createSubTableRow(param_table, key, data[key], i, false)
   }
   return param_table;
 }
 
 function tableFromJson() {
   let i;
-  var col = [];
+  let col = [];
   for (i = 0; i < inspectElementList.length; i++) {
-    for (var key in inspectElementList[i]) {
+    for (let key in inspectElementList[i]) {
       if (col.indexOf(key) === -1) {
         col.push(key);
       }
@@ -253,8 +222,8 @@ function tableFromJson() {
   table.setAttribute('cellspacing', '0');
 
   // Create table header row using the extracted headers above.
-  var head = table.createTHead();
-  var tr = head.insertRow(-1);                    // table row.
+  let head = table.createTHead();
+  let tr = head.insertRow(-1);                    // table row.
   // tr.setAttribute('style','d-flex');
 
   // table header.
@@ -267,14 +236,14 @@ function tableFromJson() {
     tr.appendChild(th);
   }
 
-  var body = table.createTBody();
+  let body = table.createTBody();
   // add json data to the table as rows.
   for (i = 0; i < inspectElementList.length; i++) {
 
     tr = body.insertRow(-1);
 
-    for (var j = 0; j < col.length; j++) {
-      var tabCell = tr.insertCell(-1);
+    for (let j = 0; j < col.length; j++) {
+      let tabCell = tr.insertCell(-1);
       if (col[j] === "actions") {
         tabCell.appendChild(createDeleteButton(i));
         tabCell.appendChild(createEditButton(i));
@@ -283,19 +252,19 @@ function tableFromJson() {
       } else if(col[j] === "param") {
         const sub_table = createSubTable(inspectElementList[i][col[j]], i);
         tabCell.appendChild(sub_table);
+      } else if (col[j] === "command") {
+        const cmdDropdown = createSelectElement(cmd, false)
+        cmdDropdown.setAttribute('id', (col[j] + '_' + i))
+        cmdDropdown.value = inspectElementList[i][col[j]];
+        tabCell.appendChild(cmdDropdown);
       } else {
-        tabCell.setAttribute('id', (col[j] + '_' + i))
-        var cellText = inspectElementList[i][col[j]];
-        if (cellText && cellText.length > 20) { cellText = cellText.substring(0,20) + '...';}
-        tabCell.innerHTML = cellText;
+        tabCell.innerHTML = inspectElementList[i][col[j]];
       }
     }
   }
 
   // Now, add the newly created table with json data, to a container.
-  var divShowData = document.getElementById('showData');
+  let divShowData = document.getElementById('showData');
   divShowData.innerHTML = "";
   divShowData.appendChild(table);
-      
-  // document.getElementById('msg').innerHTML = '<br />You can later <a href="https://www.encodedna.com/javascript/dynamically-add-remove-rows-to-html-table-using-javascript-and-save-data.htm" target="_blank" style="color:#1464f4;text-decoration:none;">get all the data from table and save it in a database.</a>';
 }
