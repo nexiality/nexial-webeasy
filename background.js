2a8a5f24644900301c54d82d05a56a55bef5267b
Divya Srivastava <srivastava.divya7@gmail.com>
2021-05-12 01:51:28 +0530

    update():sequence

--- background.js
+++ background.js
@@ -1,39 +1,9 @@
-console.log('background running');
 var is_inspecting = 'stop';
 var inspectElementList = [];
 var currentTab = '';
 
-chrome.tabs.onActivated.addListener(tab => {
-  // console.log(tab);
-  chrome.tabs.get(tab.tabId, current_tab_info => {
-    // console.log(current_tab_info.url);
-    currentTab = current_tab_info.url;
-    //match the url with nexial url
-    if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
-      //toDo
-      //add context menu if its nexial to downlad json
-      //add button to copy or download json
-      //clipbord* (tab) ... save in cookie-, file*(tab space) txt csv
-      // console.log('Its google page')
-      // inject css
-      // chrome.tabs.insertCSS(null, {file: 'mystyles.css'});
-    }
-  });
-});
-
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  console.log('chrome.tabs.onUpdated - is_inspecting  =  ', is_inspecting);
-  if (is_inspecting === 'start' && changeInfo.status === 'complete') {
-    // console.log('LOAD executeScript: eventRecorder')
-    // chrome.tabs.executeScript(null, {file: '/inspection/utils.js'}, () => chrome.runtime.lastError);
-    chrome.tabs.executeScript(null, {file: '/inspection/eventInspecting.js'}, () => chrome.runtime.lastError);
-  }
-})
-
 function loadListener(url) {
   inspectElementList.push({step: 1, command: 'open(url)', param: {url: url}, actions: ''});
-  // console.log('first entry : ', inspectElementList)
-  // console.log('LOAD executeScript: eventRecorder')
   chrome.tabs.executeScript(null, {file: '/inspection/eventInspecting.js'}, function (result) {
     // Process |result| here (or maybe do nothing at all).
     // console.log('execute script : ', result)
@@ -55,6 +25,35 @@ function createOpenURLEntry(url) {
   }
 }
 
+// chrome.tabs.onActivated.addListener(tab => {
+//   // console.log(tab);
+//   chrome.tabs.get(tab.tabId, current_tab_info => {
+//     // console.log(current_tab_info.url);
+//     currentTab = current_tab_info.url;
+//     //match the url with nexial url
+//     if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
+//       //toDo
+//       //add context menu if its nexial to downlad json
+//       //add button to copy or download json
+//       //clipbord* (tab) ... save in cookie-, file*(tab space) txt csv
+//       // console.log('Its google page')
+//       // inject css
+//       // chrome.tabs.insertCSS(null, {file: 'mystyles.css'});
+//     }
+//   });
+// });
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  console.log('changeInfo.status = ', changeInfo.status)
+  if (is_inspecting === 'start' && changeInfo.status === 'complete') {
+    console.log('chrome.tabs.onUpdated - is_inspecting  =  ', is_inspecting);
+    console.log('tabId = ', tabId);
+    console.log('tab = ', tab);
+    // chrome.tabs.executeScript(null, {file: '/inspection/utils.js'}, () => chrome.runtime.lastError);
+    chrome.tabs.executeScript(null, {file: '/inspection/eventInspecting.js'}, () => chrome.runtime.lastError);
+  }
+})
+
 chrome.runtime.onMessage.addListener(function (action, sender, sendResponse) {
   switch (action.cmd) {
     case 'start_inspecting': {
@@ -70,6 +69,7 @@ chrome.runtime.onMessage.addListener(function (action, sender, sendResponse) {
       break;
     }
     case 'inspecting': {
+      console.log(action.value)
       inspectElementList.push(action.value);
       break;
     }
