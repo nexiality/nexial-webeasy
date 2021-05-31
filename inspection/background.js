10d81c93f5671e7701346ca8c715723f61008bb2
Divya Srivastava <srivastava.divya7@gmail.com>
2021-05-31 17:20:31 +0530

    fix(): badge on inspecting tab

--- inspection/background.js
+++ inspection/background.js
@@ -1,14 +1,16 @@
 var is_inspecting = 'stop';
 var inspectElementList = [];
-var currentTab = '';
+var currentTab = null;
+var inspectingTab = null;
 // Add a `manifest` property to the `chrome` object.
 chrome.manifest = chrome.app.getDetails();
 
 function updateBadge() {
   if (is_inspecting === 'start') {
-    chrome.browserAction.setBadgeText({ text: ' ' });
+    console.log(inspectingTab, "%%%%%%%%%%%%%%%%%%%%%%%")
     chrome.browserAction.setBadgeBackgroundColor({ color: 'red' });
-  } else   chrome.browserAction.setBadgeText({ text: '' });
+    chrome.browserAction.setBadgeText({ tabId: inspectingTab.tabId, text: ' ' });
+  } else   chrome.browserAction.setBadgeText({ tabId: inspectingTab.tabId, text: '' });
 }
 
 function loadListener(url) {
@@ -23,13 +25,15 @@ function createOpenURLEntry(url) {
   if (url) {
     chrome.tabs.create({"url": url}, function (tab) {
       // console.log('given url is open', url)
+      inspectingTab['url'] = url;
       loadListener(url)
     });
   } else {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
       if (!tabs || tabs.length < 1) return;
-      currentTab = tabs[0].url;
-      loadListener(currentTab);
+      //Todo check on new tab open 
+      inspectingTab['url'] = tabs[0].url;
+      loadListener(tabs[0].url);
     });
   }
 }
@@ -60,47 +64,57 @@ chrome.windows.getAll({
   var i = 0, w = windows.length, currentWindow;
   for( ; i < w; i++ ) {
       currentWindow = windows[i];
-      var j = 0, t = currentWindow.tabs.length, currentTab;
+      var j = 0, t = currentWindow.tabs.length, currentWindowTab;
       for( ; j < t; j++ ) {
-          currentTab = currentWindow.tabs[j];
+        currentWindowTab = currentWindow.tabs[j];
+          // console.log('CURRENT TAB ==== ', currentWindowTab)
+          // console.log('CURRENT TAB URL ==== ', currentWindowTab.url)
           // Skip chrome:// and https:// pages
-          if( currentTab.url && ! currentTab.url.match(/(chrome|https):\/\//gi) ) {
-              injectIntoTab(currentTab);
+          if( currentWindowTab.url && ! currentWindowTab.url.match(/(chrome|https):\/\//gi) ) {
+              injectIntoTab(currentWindowTab);
           }
       }
   }
 });
 
-// chrome.tabs.onActivated.addListener(tab => {
-//   // console.log(tab);
-//   chrome.tabs.get(tab.tabId, current_tab_info => {
-//     // console.log(current_tab_info.url);
-//     currentTab = current_tab_info.url;
-//     //match the url with nexial url
-//     if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
-//       //toDo
-//       //add context menu if its nexial to downlad json
-//       //add button to copy or download json
-//       //clipbord* (tab) ... save in cookie-, file*(tab space) txt csv
-//       // console.log('Its google page')
-//       // inject css
-//       // chrome.tabs.insertCSS(null, {file: 'mystyles.css'});
-//     }
-//   });
-// });
+chrome.tabs.onActivated.addListener(tab => {
+  console.log('onActivated currentTab == ', tab);
+  currentTab = tab;
+  // chrome.tabs.get(tab.tabId, current_tab_info => {
+  //   console.log('current_tab_info ---------- ', current_tab_info.url);
+  //   currentTab = current_tab_info.url;
+  //   //match the url with nexial url
+  //   if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
+  //     //toDo
+  //     //add context menu if its nexial to downlad json
+  //     //add button to copy or download json
+  //     //clipbord* (tab) ... save in cookie-, file*(tab space) txt csv
+  //     // console.log('Its google page')
+  //     // inject css
+  //     // chrome.tabs.insertCSS(null, {file: 'mystyles.css'});
+  //   }
+  // });
+});
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  console.log('changeInfo.status = ', changeInfo.status)
   if (is_inspecting === 'start' && changeInfo.status === 'complete') {
+    // console.log('chrome.tabs.onUpdated - is_inspecting  =  ', is_inspecting);
+    console.log('tabId = ', tabId);
+    console.log('onUpdated currentTab == ', currentTab, ' changeInfo = ', changeInfo, ' tab = ', tab)
+    // console.log('tab = ', tab);
     // chrome.tabs.executeScript(null, {file: '/inspection/utils.js'}, () => chrome.runtime.lastError);
     // chrome.tabs.executeScript(null, {file: '/inspection/eventInspecting.js'}, () => chrome.runtime.lastError);
   }
 })
 
 chrome.runtime.onMessage.addListener(function (action, sender, sendResponse) {
+  console.log('current TAb == ', currentTab)
   switch (action.cmd) {
     case 'start_inspecting': {
       inspectElementList = [];
       is_inspecting = 'start';
+      inspectingTab = JSON.parse(JSON.stringify(currentTab));
       createOpenURLEntry(action.value);
       sendResponse({msg: 'start inspecting'});
       sendRunTimeMessage({action: 'start'})
