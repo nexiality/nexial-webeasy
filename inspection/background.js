f59c0b0e8a5d5905bf6307d47aef1b8dfe1dc509
Divya Srivastava <srivastava.divya7@gmail.com>
2021-05-31 17:49:37 +0530

    fix():badge after update

--- inspection/background.js
+++ inspection/background.js
@@ -100,7 +100,8 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   console.log('changeInfo.status = ', changeInfo.status)
   if (is_inspecting === 'start' && changeInfo.status === 'complete') {
     // console.log('chrome.tabs.onUpdated - is_inspecting  =  ', is_inspecting);
-      sendRunTimeMessage({action: 'start'})
+      sendRunTimeMessage({action: 'start'});
+      updateBadge();
     console.log('tabId = ', tabId);
     console.log('onUpdated currentTab == ', currentTab, ' changeInfo = ', changeInfo, ' tab = ', tab)
     // console.log('tab = ', tab);
