81aec07577757467a45b44b0d6c6fc86bd3b60fd
Divya Srivastava <srivastava.divya7@gmail.com>
2021-06-09 17:59:35 +0530

    last left commit

---
+++ resources/scripts/console.js
@@ -0,0 +1,42 @@
+function sendConsole(level, logging) {
+  const payload = {
+    cmd  : 'console',
+    data: logging,
+    type: level
+  };
+
+  if (!chrome || !chrome.runtime || !payload) return;
+  chrome.runtime.sendMessage(payload);
+}
+
+function printLog(level, logging) {
+  switch (level) {
+    case 'group':
+      console.group(logging)
+      break;
+    case 'groupEnd':
+      console.groupEnd(logging)
+      break;
+    case 'time':
+      console.time(logging)
+      break;
+    case 'timeEnd':
+      console.timeEnd(logging)
+      break;
+    case 'log':
+      console.log(logging)
+      break;
+    case 'info':
+      console.info(logging)
+      break;
+    case 'warn':
+      console.warn(logging)
+      break;
+    case 'error':
+      console.error(logging)
+      break;
+    case 'table':
+      console.table(logging)
+      break;
+  }
+}
