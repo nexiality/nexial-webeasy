5f63487eda07fdccafc4ea12d0fd3465df4ace06
Divya Srivastava <srivastava.divya7@gmail.com>
2021-07-02 00:47:26 +0530

    Copy textbox

--- inspection/contentScript.js
+++ inspection/contentScript.js
@@ -423,15 +423,18 @@ function getLocatorElement(items, editable = true) {
 
 function createLocatorDialog(locator) {
   document.body.innerHTML += `
-  <dialog class="">
-    <div class="">
-      <div class="show-locator-header">Show Selector</div>
-      <button type="button" class="close" data-dismiss="modal"> × </button>
+  <dialog class="nexial-dialog-container">
+    <div class="show-locator-header">Show Selector</div>
+    <button type="button" class="close" data-dismiss="modal"> × </button>
+    <div class="locator-content">
+    </div>
+    <div class="copy-content">
+    <textarea class="form-control nexial-selected-locator"> </textarea>
     </div>
   </dialog>
   `;
 
-  let dialog = document.querySelector("dialog");
+  let dialog = document.getElementsByClassName("nexial-dialog-container")[0];
   dialog.style.position = "absolute";
   dialog.style.position = "relative";
   dialog.style.border = "1px solid #999";
@@ -440,9 +443,16 @@ function createLocatorDialog(locator) {
   dialog.getElementsByClassName("show-locator-header")[0].style.display = "inline-block";
   dialog.getElementsByClassName("close")[0].style.float = "right";
 
+  const selectedLocator = dialog.getElementsByClassName("nexial-selected-locator")[0];
   const cmdDropdown = getLocatorElement(locator);
+  cmdDropdown.onchange = function (event) {
+    selectedLocator.value = '';
+    selectedLocator.value = event.target.value;
+    console.log(event, '#############')
+  }
   cmdDropdown.style.margin = "25px 0px";
-  dialog.appendChild(cmdDropdown);
+  selectedLocator.style.width = "100%";
+  dialog.getElementsByClassName("locator-content")[0].appendChild(cmdDropdown);
   dialog.querySelector("button").addEventListener("click", function() {
     cmdDropdown.remove();
     dialog.close();
